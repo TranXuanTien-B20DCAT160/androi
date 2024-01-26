fd3c6c575422710fafe7b90cab4bf45991f610dd
androi <tranxuantienx2@gmail.com>
2024-01-27 02:17:31 +0700

    commit lan 1

---
+++ app/src/main/java/com/example/test_ytb/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.test_ytb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dang_nhap);
+    }
+}
